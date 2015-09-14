463f63193cac0be4391332aa5076fe24f948f774
Jamminsvinyl <cemulku@gmail.com>
2015-09-14 18:39:16 +0300

    Create search1973.js

---
+++ search1973.js
@@ -0,0 +1,490 @@
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+var indexPos = 0;
+	   var songs = [
+
+"Tie a Yellow Ribbon Round the Ole Oak Tree 	Tony Orlando and Dawn",
+"Bad, Bad Leroy Brown 	Jim Croce",
+"Killing Me Softly with His Song 	Roberta Flack",
+"Let's Get It On 	Marvin Gaye",
+"My Love 	Paul McCartney & Wings",
+"Why Me 	Kris Kristofferson",
+"Crocodile Rock 	Elton John",
+"Will It Go Round in Circles 	Billy Preston",
+"You're So Vain 	Carly Simon",
+"Touch Me in the Morning 	Diana Ross",
+"The Night the Lights Went Out in Georgia 	Vicki Lawrence",
+"Playground In My Mind 	Clint Holmes",
+"Brother Louie 	Stories",
+"Delta Dawn 	Helen Reddy",
+"Me and Mrs. Jones 	Billy Paul",
+"Frankenstein 	Edgar Winter Group",
+"Drift Away 	Dobie Gray",
+"Little Willy 	Sweet",
+"You Are the Sunshine of My Life 	Stevie Wonder",
+"Half-Breed 	Cher",
+"That Lady 	The Isley Brothers",
+"Pillow Talk 	Sylvia",
+"We're an American Band 	Grand Funk Railroad",
+"Right Place Wrong Time 	Dr. John",
+"Wildflower 	Skylark",
+"Superstition 	Stevie Wonder",
+"Loves Me Like a Rock 	Paul Simon",
+"The Morning After 	Maureen McGovern",
+"Rocky Mountain High 	John Denver",
+"Stuck in the Middle with You 	Stealers Wheel",
+"Shambala 	Three Dog Night",
+"Love Train 	The O'Jays",
+"I'm Gonna Love You Just a Little More Baby 	Barry White",
+"Say, Has Anybody Seen My Sweet Gypsy Rose 	Tony Orlando and Dawn",
+"Keep on Truckin' 	Eddie Kendricks",
+"Dancing in the Moonlight 	King Harvest",
+"Danny's Song 	Anne Murray",
+"Monster Mash 	Bobby "Boris" Pickett and the Crypt-Kickers",
+"Natural High 	Bloodstone",
+"Diamond Girl 	Seals and Crofts",
+"Long Train Runnin' 	The Doobie Brothers",
+"Give Me Love (Give Me Peace on Earth) 	George Harrison",
+"If You Want Me to Stay 	Sly & the Family Stone",
+"Daddy's Home 	Jermaine Jackson",
+"Neither One of Us (Wants to Be the First to Say Goodbye) 	Gladys Knight & the Pips",
+"I'm Doin' Fine Now 	New York City",
+"Could It Be I'm Falling in Love 	The Spinners",
+"Daniel 	Elton John",
+"Midnight Train to Georgia 	Gladys Knight & the Pips",
+"Smoke on the Water 	Deep Purple",
+"The Cover of the Rolling Stone 	Dr. Hook & The Medicine Show",
+"Behind Closed Doors 	Charlie Rich",
+"Your Mama Don't Dance 	Loggins and Messina",
+"Feelin' Stronger Every Day 	Chicago",
+"The Cisco Kid 	War",
+"Live and Let Die 	Paul McCartney & Wings",
+"Oh, Babe, What Would You Say? 	Hurricane Smith",
+"I Believe in You (You Believe in Me) 	Johnnie Taylor",
+"Sing 	The Carpenters",
+"Ain't No Woman (Like the One I've Got) 	The Four Tops",
+"Dueling Banjos 	Eric Weissberg & Steve Mandell",
+"Higher Ground 	Stevie Wonder",
+"Here I Am (Come And Take Me) 	Al Green",
+"My Maria 	B.W. Stevenson",
+"Superfly 	Curtis Mayfield",
+"Get Down 	Gilbert O'Sullivan",
+"Last Song 	Edward Bear",
+"Reelin' In the Years 	Steely Dan",
+"Hocus Pocus 	Focus",
+"Yesterday Once More 	The Carpenters",
+"Boogie Woogie Bugle Boy 	Bette Midler",
+"Clair 	Gilbert O'Sullivan",
+"Do It Again 	Steely Dan",
+"Kodachrome 	Paul Simon",
+"Why Can't We Live Together 	Timmy Thomas",
+"So Very Hard to Go 	Tower of Power",
+"Do You Wanna Dance? 	Bette Midler",
+"Rockin' Pneumonia and the Boogie Woogie Flu 	Johnny Rivers",
+"Ramblin' Man 	The Allman Brothers Band",
+"Masterpiece 	The Temptations",
+"Peaceful 	Helen Reddy",
+"One of a Kind (Love Affair) 	The Spinners",
+"Funny Face 	Donna Fargo",
+"Funky Worm 	Ohio Players",
+"Angie 	The Rolling Stones",
+"Jambalaya (On the Bayou) 	Blue Ridge Rangers",
+"Don't Expect Me to Be Your Friend 	Lobo",
+"Break Up to Make Up 	The Stylistics",
+"Daisy a Day 	Jud Strunk",
+"Also Sprach Zarathustra (2001) 	Deodato",
+"Stir It Up 	Johnny Nash",
+"Money 	Pink Floyd",
+"Gypsy Man 	War",
+"The World Is a Ghetto 	War",
+"Yes We Can Can 	The Pointer Sisters",
+"Free Ride 	Edgar Winter Group",
+"Space Oddity 	David Bowie",
+"It Never Rains in Southern California 	Albert Hammond",
+"The Twelfth of Never 	Donny Osmond",
+"Papa Was a Rollin' Stone 	The Temptations",
+"Dünya Dönüyor 	Nilüfer",
+"Hey Koca Topçu	Barış Manço",
+"Kimler Geldi Kimler Geçti	Nilüfer",
+"Göreceksin Kendini 	Nilüfer",
+"Deniz Üstü Köpürür	Edip Akbayram",
+"Tek Başına	Ayten Alpman",
+"Dumanlı Dumanlı Oy Bizim Eller	Edip Akbayram",
+"Can Karagözlüm	Alpay",
+"Hayat Bayram Olsa	Şenay",
+"Kalenin Bayır Düzü 	Alpay",
+"Senden Başka	Füsun Önal",
+"Obur Dünya	Cem Karaca  Moğollar",
+"Gel Gel	Cem Karaca Moğollar",
+"Arkadaş Dur Bekle	Nilüfer  Tanju Okan ",
+"Oh Olsun	Füsun Önal",
+"Gönül Dağı 	Barış Manço",
+"İntizar	Yurdaer Doğulu",
+"Altın Kafes	Selda Bağcan",
+"Kalk Gidelim Küheylan	Barış Manço",
+"Ağlıyorum Yine	Nilüfer",
+"Avuçlarımda Hala Sıcaklığın Var	Zafer Dilek",
+"Ağlamak Geliyor İçimden	Modern Folk Üçlüsü",
+"Birleşsin Bütün Eller	İlhan İrem",
+"Aşkın Kanunu	Esin Engin",
+"Koy Koy Koy	Tanju Okan",
+"Türküz Türkü Çağırırız	Mazhar - Fuat"
+
+ 
+
+    ];
+
+var names = [
+
+"Tie a Yellow Ribbon Round the Ole Oak Tree ",
+"Bad, Bad Leroy Brown ",
+"Killing Me Softly with His Song ",
+"Let's Get It On ",
+"My Love ",
+"Why Me ",
+"Crocodile Rock ",
+"Will It Go Round in Circles ",
+"You're So Vain ",
+"Touch Me in the Morning ",
+"The Night the Lights Went Out in Georgia ",
+"Playground In My Mind ",
+"Brother Louie ",
+"Delta Dawn ",
+"Me and Mrs. Jones ",
+"Frankenstein ",
+"Drift Away ",
+"Little Willy ",
+"You Are the Sunshine of My Life ",
+"Half-Breed ",
+"That Lady ",
+"Pillow Talk ",
+"We're an American Band ",
+"Right Place Wrong Time ",
+"Wildflower ",
+"Superstition ",
+"Loves Me Like a Rock ",
+"The Morning After ",
+"Rocky Mountain High ",
+"Stuck in the Middle with You ",
+"Shambala ",
+"Love Train ",
+"I'm Gonna Love You Just a Little More Baby ",
+"Say, Has Anybody Seen My Sweet Gypsy Rose ",
+"Keep on Truckin' ",
+"Dancing in the Moonlight ",
+"Danny's Song ",
+"Monster Mash ",
+"Natural High ",
+"Diamond Girl ",
+"Long Train Runnin' ",
+"Give Me Love (Give Me Peace on Earth) ",
+"If You Want Me to Stay ",
+"Daddy's Home ",
+"Neither One of Us (Wants to Be the First to Say Goodbye) ",
+"I'm Doin' Fine Now ",
+"Could It Be I'm Falling in Love ",
+"Daniel ",
+"Midnight Train to Georgia ",
+"Smoke on the Water ",
+"The Cover of the Rolling Stone ",
+"Behind Closed Doors ",
+"Your Mama Don't Dance ",
+"Feelin' Stronger Every Day ",
+"The Cisco Kid ",
+"Live and Let Die ",
+"Oh, Babe, What Would You Say? ",
+"I Believe in You (You Believe in Me) ",
+"Sing ",
+"Ain't No Woman (Like the One I've Got) ",
+"Dueling Banjos ",
+"Higher Ground ",
+"Here I Am (Come And Take Me) ",
+"My Maria ",
+"Superfly ",
+"Get Down ",
+"Last Song ",
+"Reelin' In the Years ",
+"Hocus Pocus ",
+"Yesterday Once More ",
+"Boogie Woogie Bugle Boy ",
+"Clair ",
+"Do It Again ",
+"Kodachrome ",
+"Why Can't We Live Together ",
+"So Very Hard to Go ",
+"Do You Wanna Dance? ",
+"Rockin' Pneumonia and the Boogie Woogie Flu ",
+"Ramblin' Man ",
+"Masterpiece ",
+"Peaceful ",
+"One of a Kind (Love Affair) ",
+"Funny Face ",
+"Funky Worm ",
+"Angie ",
+"Jambalaya (On the Bayou) ",
+"Don't Expect Me to Be Your Friend ",
+"Break Up to Make Up ",
+"Daisy a Day ",
+"Also Sprach Zarathustra (2001) ",
+"Stir It Up ",
+"Money ",
+"Gypsy Man ",
+"The World Is a Ghetto ",
+"Yes We Can Can ",
+"Free Ride ",
+"Space Oddity ",
+"It Never Rains in Southern California ",
+"The Twelfth of Never ",
+"Papa Was a Rollin' Stone ",
+"Dünya Dönüyor ",
+"Hey Koca Topçu",
+"Kimler Geldi Kimler Geçti",
+"Göreceksin Kendini ",
+"Deniz Üstü Köpürür",
+"Tek Başına",
+"Dumanlı Dumanlı Oy Bizim Eller",
+"Can Karagözlüm",
+"Hayat Bayram Olsa",
+"Kalenin Bayır Düzü ",
+"Senden Başka",
+"Obur Dünya",
+"Gel Gel",
+"Arkadaş Dur Bekle",
+"Oh Olsun",
+"Gönül Dağı ",
+"İntizar",
+"Altın Kafes",
+"Kalk Gidelim Küheylan",
+"Ağlıyorum Yine",
+"Avuçlarımda Hala Sıcaklığın Var",
+"Ağlamak Geliyor İçimden",
+"Birleşsin Bütün Eller",
+"Aşkın Kanunu",
+"Koy Koy Koy",
+"Türküz Türkü Çağırırız"
+
+
+
+
+ ];
+
+ var artist = [
+
+"Tony Orlando and Dawn",
+"Jim Croce",
+"Roberta Flack",
+"Marvin Gaye",
+"Paul McCartney & Wings",
+"Kris Kristofferson",
+"Elton John",
+"Billy Preston",
+"Carly Simon",
+"Diana Ross",
+"Vicki Lawrence",
+"Clint Holmes",
+"Stories",
+"Helen Reddy",
+"Billy Paul",
+"Edgar Winter Group",
+"Dobie Gray",
+"Sweet",
+"Stevie Wonder",
+"Cher",
+"The Isley Brothers",
+"Sylvia",
+"Grand Funk Railroad",
+"Dr. John",
+"Skylark",
+"Stevie Wonder",
+"Paul Simon",
+"Maureen McGovern",
+"John Denver",
+"Stealers Wheel",
+"Three Dog Night",
+"The O'Jays",
+"Barry White",
+"Tony Orlando and Dawn",
+"Eddie Kendricks",
+"King Harvest",
+"Anne Murray",
+"Bobby "Boris" Pickett and the Crypt-Kickers",
+"Bloodstone",
+"Seals and Crofts",
+"The Doobie Brothers",
+"George Harrison",
+"Sly & the Family Stone",
+"Jermaine Jackson",
+"Gladys Knight & the Pips",
+"New York City",
+"The Spinners",
+"Elton John",
+"Gladys Knight & the Pips",
+"Deep Purple",
+"Dr. Hook & The Medicine Show",
+"Charlie Rich",
+"Loggins and Messina",
+"Chicago",
+"War",
+"Paul McCartney & Wings",
+"Hurricane Smith",
+"Johnnie Taylor",
+"The Carpenters",
+"The Four Tops",
+"Eric Weissberg & Steve Mandell",
+"Stevie Wonder",
+"Al Green",
+"B.W. Stevenson",
+"Curtis Mayfield",
+"Gilbert O'Sullivan",
+"Edward Bear",
+"Steely Dan",
+"Focus",
+"The Carpenters",
+"Bette Midler",
+"Gilbert O'Sullivan",
+"Steely Dan",
+"Paul Simon",
+"Timmy Thomas",
+"Tower of Power",
+"Bette Midler",
+"Johnny Rivers",
+"The Allman Brothers Band",
+"The Temptations",
+"Helen Reddy",
+"The Spinners",
+"Donna Fargo",
+"Ohio Players",
+"The Rolling Stones",
+"Blue Ridge Rangers",
+"Lobo",
+"The Stylistics",
+"Jud Strunk",
+"Deodato",
+"Johnny Nash",
+"Pink Floyd",
+"War",
+"War",
+"The Pointer Sisters",
+"Edgar Winter Group",
+"David Bowie",
+"Albert Hammond",
+"Donny Osmond",
+"The Temptations",
+"Nilüfer",
+"Barış Manço",
+"Nilüfer",
+"Nilüfer",
+"Edip Akbayram",
+"Ayten Alpman",
+"Edip Akbayram",
+"Alpay",
+"Şenay",
+"Alpay",
+"Füsun Önal",
+"Cem Karaca  Moğollar",
+"Cem Karaca Moğollar",
+"Nilüfer  Tanju Okan ",
+"Füsun Önal",
+"Barış Manço",
+"Yurdaer Doğulu",
+"Selda Bağcan",
+"Barış Manço",
+"Nilüfer",
+"Zafer Dilek",
+"Modern Folk Üçlüsü",
+"İlhan İrem",
+"Esin Engin",
+"Tanju Okan",
+"Mazhar - Fuat"
+
+
+
+
+];
+
+
+
+
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See http://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('AIzaSyCBzjxLmrI6-HCQbaKbyWkBQuplm2ESpCU');
+
+ 
+
+ //    var arrayLength = songs.length;
+	
+	// for (var i = 0; i < arrayLength; i++) {
+	// var raw = songs[i];
+	// var clean = raw.replace(/\W/g, '');
+
+ //    search(clean);
+	// }
+
+
+ //   search(	"Thrift Shop Macklemore and Ryan Lewis featuring Wanz");
+ 	  searchNext();
+}
+
+
+function search(query) {
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: query,
+        type: 'video',
+        maxResults: 1
+  
+
+    });
+    
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
+
+}
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+   var responseString = JSON.stringify(response, '', 2);
+   //document.getElementById('response').innerHTML += responseString;
+   var data = JSON.parse(responseString);
+
+   var videoId = (data.items[0].id.videoId);
+   var one = "&lt;div class=\"vid\" onclick=\"loadVideo($(this));\" data-id=\"";
+   var two = "\"&gt;&lt;a href=\"https://www.youtube.com/watch?v=";
+   var three = "\"&gt;&lt;/a&gt;&lt;div&gt;&lt;h3&gt;";
+   var four = "&lt;br&gt;"
+   var five	= "&lt;span&gt;"
+   var six = "&lt;/span&gt;&lt;/h3&gt;&lt;/div&gt;&lt;/div&gt;\n";
+
+   var linkElement = one + videoId + two + videoId + three  + names[indexPos-1] + four + five + artist[indexPos-1] + six  ;
+   //alert(linkElement);
+   document.getElementById('response').innerHTML += linkElement;
+   searchNext();
+
+}
+
+function searchNext(){
+
+
+    if (songs.length != indexPos){
+    	search(songs[indexPos]);
+		indexPos++;
+    }else{
+    	alert("SWAG");
+    }
+
+
+}
